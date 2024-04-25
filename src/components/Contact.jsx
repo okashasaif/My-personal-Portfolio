1a66b554eb24f07132b7a8fb9f20838d8876748d
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-04-25 22:20:25 +0500

    build

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_9x7dmwi",
+        "template_2qmo5yv",
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "okashaahsan@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "HdNlDinzYXEaYrxp4"
       )
       .then(
         () => {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Software Developer <br className='sm:block hidden' />
-            interfaces and web applications
+            
           </p>
         </div>
       </div>
